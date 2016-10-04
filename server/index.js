2c9f8083de65e10a9002671381f862d901163c95
jessica-jordan <jessica.jordan.fe.dev@gmail.com>
2016-10-04 16:40:00 +0100

    exceljs magic and most beautiful nodejs event action on the planet

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ const fs = require('fs');
 
 module.exports = function(app) {
   var globSync   = require('glob').sync;
+  const execFile = require('child_process').execFile;
+  const jsonFromExcel = require('../server/createjsonfromexcel');
+  const formatJson = require('../server/createjson');
+  const mainEmitter = formatJson.mEvents;
   var mocks      = globSync('./mocks/**/*.js', { cwd: __dirname }).map(require);
   var proxies    = globSync('./proxies/**/*.js', { cwd: __dirname }).map(require);
 
@@ -20,10 +24,28 @@ module.exports = function(app) {
   app.use(morgan('dev'));
 
   app.get('/api/mutations', function(req, res) {
-    const json = JSON.parse(fs.readFileSync('server/data/mutationmod.json'));
-    res.header("Content-Type","application/vnd.api+json"); 
-    res.send(json);
+    /*const preprocessData = execFile('./createjsonfromexcel.js', (error, stdout, stderr) => {
+      if (error){
+        throw error;
+      }
+      console.log(stdout);
+    });*/
+    mainEmitter.on('hola', () => {
+      const json = JSON.parse(fs.readFileSync('server/data/excelmod.json'));
+      res.header("Content-Type","application/vnd.api+json"); 
+      res.send(json);
+    });
+    jsonFromExcel.create(formatJson.create);
+   // formatJson.create();
+
   });
+
+  app.get('/api/interactions', function(req,res) {
+    const inst = fs.readFileSync('server/data/interactions.tsv');
+    res.send(inst);
+  });
+  
+  
 
   mocks.forEach(function(route) { route(app); });
   proxies.forEach(function(route) { route(app); });
