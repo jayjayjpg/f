ef9d39393efe9b9cb618172c26c1d48fc983b515
jessica-jordan <jessica.jordan.fe.dev@gmail.com>
2016-10-12 15:28:16 +0100

    test how tofilter mutationmatrix data for single snp

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-   return this.get('store').findAll('mutation');
+    var mutations = this.get('store').query('mutation', {
+        rsId: 'rs2425019'
+    }).then(function(snps){
+      return snps;
+    });
+    return mutations;
   }
 });
