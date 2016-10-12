6ebb7a321517caeef0747e8e94d9a70c34379e10
jessica-jordan <jessica.jordan.fe.dev@gmail.com>
2016-10-12 18:33:40 +0100

    use url query parameters for full route and model reload with filtering

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,20 +2,26 @@ import Ember from 'ember';
 import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
-  model(){
+  queryParams: {
+    regionType: {
+      refreshModel: true
+    }
+  },
+  model(param){
       var self = this;
-      /* return RSVP.hash({
-        snps: this.get('store').query('snp',{
-          region: 'missense'
-        }),
-        mutations: this.get('store').query('mutation',{
-          rsId: 'rs2425019'
-        })
-      }); */
+      console.log("fab model param: " + JSON.stringify(param));
+      if (param.regionType == null){
+        return RSVP.hash({
+          snps: this.get('store').query('snp',{
+            region: 'missense'
+          }),
+          mutations: this.get('store').findAll('mutation')
+        }); 
+      }
       // can now filter for SNPs with different region types
       return RSVP.hash({
           snps: this.get('store').query('snp',{
-            region: 'intergenic'
+            region: param.regionType // TODO: access the region parameter by model param
           })
         }).then(function(results){
            var filteredSnpIds = results.snps.mapBy('rsId').join(",");
