a85c07561aff52909278cb53a8b6071e271a17a3
jessica-jordan <jessica.jordan.fe.dev@gmail.com>
2016-10-12 18:13:22 +0100

    adapt model response to be able to filter for snps by genomic region type

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,7 @@ import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
   model(){
+      var self = this;
       /* return RSVP.hash({
         snps: this.get('store').query('snp',{
           region: 'missense'
@@ -11,19 +12,23 @@ export default Ember.Route.extend({
           rsId: 'rs2425019'
         })
       }); */
+      // can now filter for SNPs with different region types
       return RSVP.hash({
           snps: this.get('store').query('snp',{
-            region: 'missense'
-          }),
-          mutations: this.get('store').query('mutation',{
-            rsId: 'rs2425019'
+            region: 'intergenic'
           })
         }).then(function(results){
-           var filteredSnpIds = results.snps.mapBy('rsId');
+           var filteredSnpIds = results.snps.mapBy('rsId').join(",");
            console.log("rsvp results: " + filteredSnpIds);
+           return RSVP.hash({
+              snps: results.snps,
+              mutations: self.get('store').query('mutation',{
+                rsId: filteredSnpIds
+              })
+           });
+        });
           /* return this.get('store').query('mutation', {
              rsId: filteredSnpIds.join(",")
            }) */
-        });
   }
 });
