ccda4e777d26ad42c4d8b67cb21dcbb48b50e729
jessica-jordan <jessica.jordan.fe.dev@gmail.com>
2016-12-08 13:49:00 +0000

    remove mockfiles and add production variables

--- config/environment.js
+++ config/environment.js
@@ -39,7 +39,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.host = 'https://facade-backend.herokuapp.com';
   }
 
   ENV['ember-simple-auth'] = {  
